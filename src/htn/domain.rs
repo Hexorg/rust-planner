02a8357ab033975837c7853f3f92035c2dc77d5a
Hexorg <devhexorg@gmail.com>
2022-05-20 17:34:33 -0400

    Added IR optimization structure

--- src/htn/domain.rs
+++ src/htn/domain.rs
@@ -42,69 +42,86 @@ impl<'a> LinkedNode<'a, Task, TaskIterator<'a>> for Task {
     }
 }
 
+enum ExpressionType {
+    Call(String),
+    Assignment(String)
+}
+
+enum BuildContext {
+    Task(String, Option<TaskType>),
+    Expression(ExpressionType)
+}
+
+
 pub struct Domain {
     pub tasks: Vec<Task>,
-    pub main_id: usize,
+    pub world_variables: Vec<String>,
+    pub blackboard_variables: Vec<String>,
+    pub operators: Vec<String>,
 }
 
 impl Domain {
-    fn check_task_validity(stmt:&Stmt) -> Result<(),DomainError> {
-        if let Stmt::Task(task_name, _, def, _) = stmt {
-            let mut task_type:Option<TaskType> = None;
-            match def.as_ref() {
-                Stmt::Block(blk) =>
-                    for substmt in blk {
-                        match substmt {
-                            Stmt::Method(_, _, _) => match task_type {
-                                Some(TaskType::PRIMITIVE) => { return Err(DomainError{message:format!("Primitive Task {} can not have methods.", task_name)})},
-                                None => {task_type = Some(TaskType::COMPOSITE);},
-                                _ => ()
-                            },
-                            Stmt::Expression(e) => {
-                                if let Expr::Call(_, _, _) | Expr::Assignment(_, _) = e {
-                                    match task_type {
-                                        Some(TaskType::COMPOSITE) => { return Err(DomainError{message:format!("Composite Task {} can not have operator calls.", task_name)})},
-                                        None => {task_type = Some(TaskType::PRIMITIVE);},
-                                        _ => ()
-                                    }
-                                } else {
-                                    return Err(DomainError{message:format!("Primitive Task {} can only have operator calls.", task_name)})
-                                }
-                            }
-                            _ => {return Err(DomainError{message:format!("Unexpected statement inside Task {}.", task_name)})},
-                        }
-                    },
+    fn build_task(&mut self, context: &mut Vec<BuildContext>, name:&String, preconditions:&Option<Expr>, body:&Rc<Stmt>, effects:&Option<Rc<Stmt>>) -> Result<(), DomainError>  {
+        let mut build_context = Some(BuildContext::Task(name.clone(), None));
+
+        self.process_stmt(&mut build_context, body)
+
+    }
+    fn build_method(&mut self, context: &mut Vec<BuildContext>, name:&String, preconditions:&Option<Expr>, body:&Rc<Stmt>) -> Result<(), DomainError>  {
+        if let Some(BuildContext::Task(task_name, tt)) = context {
+            match tt {
+                Some(TaskType::PRIMITIVE) => return Err(DomainError{message:format!("Tasks can be either composite or primitive. Task {} is both.", task_name)}),
+                None => *tt = Some(TaskType::COMPOSITE),
                 _ => (),
             }
         }
-        Ok(())
+        self.process_stmt(context, body)
     }
-
-    fn rules_check(stmt:&Vec<Stmt>) -> Result<(),DomainError> {
-        for s in stmt {
-            Domain::check_task_validity(s)?;    
-        }
-        Ok(())
-    }
-
-    fn find_main(stmt:&Vec<Stmt>) -> Result<usize,DomainError> {
-        for (i, s) in stmt.iter().enumerate() {
-            if let Stmt::Task(name, _,_,_) = &s {
-                match name.as_str() {
-                    "Main" => return Ok(i),
-                    _ => (),
+    fn process_expr(&mut self, context:&mut Vec<BuildContext>, expr:&Expr) -> Result<(), DomainError>  {
+        Ok(match expr {
+            Expr::Binary(left, _, right) => {self.process_expr(context, left.as_ref())?; self.process_expr(context, right.as_ref())?},
+            Expr::Grouping(g) => self.process_expr(context, g.as_ref())?,
+            Expr::Literal(_) => (),
+            Expr::Variable(var_name) => match context {
+                Some(BuildContext::Expression(ExpressionType::Call(v))) => v.insert_str(0,var_name), 
+                _ => (),
+            },
+            Expr::Unary(_, right) => self.process_expr(context, right)?,
+            Expr::Assignment(_, right) => {let mut build_context = Some(BuildContext::Expression(ExpressionType::Assignment(String::new()))); 
+                self.process_expr(&mut build_context, right)?
+            },
+            Expr::Call(left, _, args) => {let mut build_context = Some(BuildContext::Expression(ExpressionType::Call(String::new()))); 
+                self.process_expr(&mut build_context, left)?; 
+                for e in args {
+                    self.process_expr(context, e)?;
                 }
-            } 
+            },
+        })
+    }
+
+    fn process_stmt(&mut self, context:&mut Vec<BuildContext>, stmt:&Stmt) -> Result<(), DomainError>  {
+        match stmt {
+            Stmt::Method(name, preconditions, body) => self.build_method(context, name, preconditions, body),
+            Stmt::Task(name, preconditions, body, effects) => self.build_task(context, name, preconditions, body, effects),
+            Stmt::Block(sub) => Ok(for item in sub.iter() { self.process_stmt(context, item)?} ),
+            Stmt::Expression(expr) => {let mut build_context = None; self.process_expr(&mut build_context, &expr)},
         }
-        Err(DomainError{message:String::from("Task 'main' not found.")})
+    }
+
+    fn pass(&mut self, ast:Vec<Stmt>) -> Result<(), DomainError> {
+        let mut build_context = Vec::new();
+        for stmt in ast {
+            self.process_stmt(&mut build_context, &stmt)?;
+        }
+        Ok(())
     }
 
     pub fn from_file(filepath:&str) -> Result<Domain, DomainError> {
         let htn_source = fs::read_to_string(filepath).expect("File error:");
         let (ast, errors) = Parser::parse(htn_source.as_str());
         Parser::print_parse_errors(errors, htn_source.as_str(), filepath);
-        Domain::rules_check(&ast)?;
-        let main_id = Domain::find_main(&ast)?;
-        Ok(Domain{ast, main_id})
+        let mut domain = Domain{ tasks:Vec::new(), world_variables:Vec::new(), blackboard_variables:Vec::new(), operators:Vec::new()};
+        domain.pass(ast)?;
+        Ok(domain)
     }
 }
